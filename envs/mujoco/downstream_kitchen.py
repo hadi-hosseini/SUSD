5230151356ffc6d45a1c7fc14916a6933d03c12e
hadi-hosseini <hadi.hosseini0171@gmail.com>
2025-07-24 21:39:27 +0000

    some parts of training high-level policy add

---
+++ envs/mujoco/downstream_kitchen.py
@@ -0,0 +1,253 @@
+from typing import Any, Optional
+
+import numpy as np
+from gymnasium import spaces
+from gymnasium.utils.ezpickle import EzPickle
+
+from gymnasium_robotics.core import GoalEnv
+from gymnasium_robotics.envs.franka_kitchen.franka_env import FrankaRobot
+
+OBS_ELEMENT_INDICES = {
+    "bottom burner": np.array([11, 12]),
+    "top burner": np.array([15, 16]),
+    "light switch": np.array([17, 18]),
+    "slide cabinet": np.array([19]),
+    "hinge cabinet": np.array([20, 21]),
+    "microwave": np.array([22]),
+    "kettle": np.array([23, 24, 25, 26, 27, 28, 29]),
+}
+OBS_ELEMENT_GOALS = {
+    "bottom burner": np.array([-0.88, -0.01]),
+    "top burner": np.array([-0.92, -0.01]),
+    "light switch": np.array([-0.69, -0.05]),
+    "slide cabinet": np.array([0.37]),
+    "hinge cabinet": np.array([0.0, 1.45]),
+    "microwave": np.array([-0.75]),
+    "kettle": np.array([-0.23, 0.75, 1.62, 0.99, 0.0, 0.0, -0.06]),
+}
+BONUS_THRESH = 0.3
+
+
+class KitchenEnv(GoalEnv, EzPickle):
+    metadata = {
+        "render_modes": [
+            "human",
+            "rgb_array",
+            "depth_array",
+        ],
+        "render_fps": 12,
+    }
+
+    def __init__(
+        self,
+        tasks_to_complete: "list[str]" = list(OBS_ELEMENT_GOALS.keys()),
+        terminate_on_tasks_completed: bool = True,
+        remove_task_when_completed: bool = True,
+        object_noise_ratio: float = 0.0005,
+        **kwargs,
+    ):
+        self.robot_env = FrankaRobot(
+            model_path="../assets/kitchen_franka/kitchen_assets/kitchen_env_model.xml",
+            **kwargs,
+        )
+
+        self.robot_env.init_qpos = np.array(
+            [
+                1.48388023e-01,
+                -1.76848573e00,
+                1.84390296e00,
+                -2.47685760e00,
+                2.60252026e-01,
+                7.12533105e-01,
+                1.59515394e00,
+                4.79267505e-02,
+                3.71350919e-02,
+                -2.66279850e-04,
+                -5.18043486e-05,
+                3.12877220e-05,
+                -4.51199853e-05,
+                -3.90842156e-06,
+                -4.22629655e-05,
+                6.28065475e-05,
+                4.04984708e-05,
+                4.62730939e-04,
+                -2.26906415e-04,
+                -4.65501369e-04,
+                -6.44129196e-03,
+                -1.77048263e-03,
+                1.08009684e-03,
+                -2.69397440e-01,
+                3.50383255e-01,
+                1.61944683e00,
+                1.00618764e00,
+                4.06395120e-03,
+                -6.62095997e-03,
+                -2.68278933e-04,
+            ]
+        )
+
+        self.model = self.robot_env.model
+        self.data = self.robot_env.data
+        self.render_mode = self.robot_env.render_mode
+
+        self.terminate_on_tasks_completed = terminate_on_tasks_completed
+        self.remove_task_when_completed = remove_task_when_completed
+
+        self.goal = {}
+        self.tasks_to_complete = set(tasks_to_complete)
+        # Validate list of tasks to complete
+        for task in tasks_to_complete:
+            if task not in OBS_ELEMENT_GOALS.keys():
+                raise ValueError(
+                    f"The task {task} cannot be found the the list of possible goals: {OBS_ELEMENT_GOALS.keys()}"
+                )
+            else:
+                self.goal[task] = OBS_ELEMENT_GOALS[task]
+
+        self.step_task_completions = (
+            []
+        )  # Tasks completed in the current environment step
+        self.episode_task_completions = (
+            []
+        )  # Tasks completed that have been completed in the current episode
+        self.object_noise_ratio = (
+            object_noise_ratio  # stochastic noise added to the object observations
+        )
+
+        robot_obs = self.robot_env._get_obs()
+        obs = self._get_obs(robot_obs)
+
+        assert (
+            int(np.round(1.0 / self.robot_env.dt)) == self.metadata["render_fps"]
+        ), f'Expected value: {int(np.round(1.0 / self.robot_env.dt))}, Actual value: {self.metadata["render_fps"]}'
+
+        self.action_space = self.robot_env.action_space
+        self.observation_space = spaces.Dict(
+            dict(
+                desired_goal=spaces.Dict(
+                    {
+                        task: spaces.Box(
+                            -np.inf,
+                            np.inf,
+                            shape=goal.shape,
+                            dtype="float64",
+                        )
+                        for task, goal in obs["achieved_goal"].items()
+                    }
+                ),
+                achieved_goal=spaces.Dict(
+                    {
+                        task: spaces.Box(
+                            -np.inf,
+                            np.inf,
+                            shape=goal.shape,
+                            dtype="float64",
+                        )
+                        for task, goal in obs["achieved_goal"].items()
+                    }
+                ),
+                observation=spaces.Box(
+                    -np.inf, np.inf, shape=obs["observation"].shape, dtype="float64"
+                ),
+            )
+        )
+
+        EzPickle.__init__(
+            self,
+            tasks_to_complete,
+            terminate_on_tasks_completed,
+            remove_task_when_completed,
+            object_noise_ratio,
+            **kwargs,
+        )
+
+    def compute_reward(
+        self,
+        achieved_goal: "dict[str, np.ndarray]",
+        desired_goal: "dict[str, np.ndarray]",
+        info: "dict[str, Any]",
+    ):
+        self.step_task_completions.clear()
+        for task in self.tasks_to_complete:
+            distance = np.linalg.norm(achieved_goal[task] - desired_goal[task])
+            complete = distance < BONUS_THRESH
+            if complete:
+                self.step_task_completions.append(task)
+
+        return float(len(self.step_task_completions))
+
+    def _get_obs(self, robot_obs):
+        obj_qpos = self.data.qpos[9:].copy()
+        obj_qvel = self.data.qvel[9:].copy()
+
+        # Simulate observation noise
+        obj_qpos += (
+            self.object_noise_ratio
+            * self.robot_env.robot_pos_noise_amp[8:]
+            * self.robot_env.np_random.uniform(low=-1.0, high=1.0, size=obj_qpos.shape)
+        )
+        obj_qvel += (
+            self.object_noise_ratio
+            * self.robot_env.robot_vel_noise_amp[9:]
+            * self.robot_env.np_random.uniform(low=-1.0, high=1.0, size=obj_qvel.shape)
+        )
+
+        achieved_goal = {
+            task: self.data.qpos[OBS_ELEMENT_INDICES[task]] for task in self.goal.keys()
+        }
+
+        obs = {
+            "observation": np.concatenate((robot_obs, obj_qpos, obj_qvel)),
+            "achieved_goal": achieved_goal,
+            "desired_goal": self.goal,
+        }
+
+        return obs
+
+    def step(self, action):
+        robot_obs, _, terminated, truncated, info = self.robot_env.step(action)
+        obs = self._get_obs(robot_obs)
+
+        reward = self.compute_reward(obs["achieved_goal"], self.goal, info)
+
+        # I comment because I don't need it.
+        
+        if self.remove_task_when_completed:
+            # When the task is accomplished remove from the list of tasks to be completed
+            [
+                self.tasks_to_complete.remove(element)
+                for element in self.step_task_completions
+            ]
+
+        info = {"tasks_to_complete": list(self.tasks_to_complete)}
+        info["step_task_completions"] = self.step_task_completions.copy()
+
+        for task in self.step_task_completions:
+            if task not in self.episode_task_completions:
+                self.episode_task_completions.append(task)
+        info["episode_task_completions"] = self.episode_task_completions
+        if self.terminate_on_tasks_completed:
+            # terminate if there are no more tasks to complete
+            terminated = len(self.episode_task_completions) == len(self.goal.keys())
+
+        return obs, reward, terminated, truncated, info
+
+    def reset(self, *, seed: Optional[int] = None, **kwargs):
+        super().reset(seed=seed, **kwargs)
+        self.episode_task_completions.clear()
+        robot_obs, _ = self.robot_env.reset(seed=seed)
+        obs = self._get_obs(robot_obs)
+        self.tasks_to_complete = set(self.goal.keys())
+        info = {
+            "tasks_to_complete": list(self.tasks_to_complete),
+            "episode_task_completions": [],
+            "step_task_completions": [],
+        }
+
+        return obs, info
+
+    def render(self):
+        return self.robot_env.render()
+
+    def close(self):
+        self.robot_env.close()
--- src/train.py
+++ src/train.py
@@ -1,110 +1,316 @@
+from dataclasses import dataclass, field
+from typing import Optional, List
+import os
 import torch
-import gym
 import numpy as np
-from stable_baselines3 import SAC
-from gym.wrappers import Monitor
-from envs.mujoco.ant_env import AntEnv
+import functools
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+import dowel_wrapper
 
-# Load pretrained option_policy and freeze weights
-checkpoint = torch.load('exp/Debug/sd000_1752248887_ant_metra/option_policy3000.pt')
-option_policy = checkpoint['policy']
-option_policy.to(device)
+assert dowel_wrapper is not None
+import dowel
 
-option_policy.eval()
-for param in option_policy.parameters():
-    param.requires_grad = False
+from iod.sac import SAC
+from iod.ppo import PPO
+from src.child_policy_env import ChildPolicyEnv
+from src.conf import DSDConfig
+from src.utils import get_exp_name, get_log_dir
+from envs.mujoco.downstream_kitchen import KitchenEnv
 
-
-class HierarchicalEnvWrapper(gym.Env):
-    def __init__(self, env, option_policy, skill_dim, skill_duration=20):
-        super().__init__()
-        self.env = env
-        self.option_policy = option_policy
-        self.skill_duration = skill_duration
-
-        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(skill_dim,), dtype=np.float32)  # for continuous skill_dim
-        self.observation_space = env.observation_space
-
-        self.current_step = 0
-        self.current_skill = None
-        self.current_obs = None  # store latest obs
-
-    def reset(self):
-        self.current_step = 0
-        self.current_skill = None
-        obs = self.env.reset()
-        self.current_obs = obs
-        return obs
-
-    def step(self, action):
-        self.current_skill = action
-        total_reward = 0.0
-        done = False
-        info = {}
-
-        for _ in range(self.skill_duration):
-            obs = self.current_obs
-            obs_tensor = torch.tensor(obs).float().unsqueeze(0).to(device)
-            skill_tensor = torch.tensor(self.current_skill).float().unsqueeze(0).to(device)
-
-            policy_input = torch.cat([obs_tensor, skill_tensor], dim=-1)
-
-            with torch.no_grad():
-                act = self.option_policy(policy_input)
-                act = act.cpu().numpy()
-
-            obs, reward, terminated, truncated, info = self.env.step(act)
-            done = terminated or truncated
-            total_reward += reward
-            self.current_obs = obs
-
-            if done:
-                break
-
-        return obs, total_reward, done, info
+from garagei.experiment.option_local_runner import OptionLocalRunner
+from garage.experiment.deterministic import set_seed
+from garage import wrap_experiment
+from garagei.torch.optimizers.optimizer_group_wrapper import OptimizerGroupWrapper
+from garage.torch.modules import MLPModule
+from garagei.sampler.option_multiprocessing_sampler import OptionMultiprocessingSampler
+from garagei.torch.q_functions.continuous_mlp_q_function_ex import ContinuousMLPQFunctionEx
+from garagei.torch.modules.parameter_module import ParameterModule
+from garagei.replay_buffer.path_buffer_ex import PathBufferEx
 
 
-if __name__ == "__main__":
-    base_env = AntEnv(render_hw=100)
+@dataclass
+class DSDHighLevelConfig(DSDConfig):
+    cp_path: Optional[str] = None
+    cp_path_idx: Optional[int] = None  # For exp name
+    cp_multi_step: int = 1
+    cp_unit_length: int = 0
 
-    # Wrap with Monitor to save videos, recordings saved to ./videos/
-    video_dir = "./videos"
-    monitored_env = Monitor(base_env, video_dir, video_callable=lambda episode_id: True, force=True)
+    downstream_reward_type: str = 'esparse'
+    downstream_num_goal_steps: int = 50
 
-    skill_dim = 12
-    skill_duration = 25
+    goal_range: float = 50.0
 
-    hier_env = HierarchicalEnvWrapper(monitored_env, option_policy, skill_dim, skill_duration)
+@dataclass
+class METRAHighLevelKitchenConfig(DSDHighLevelConfig):
+    max_path_length: int = 8
+    dim_option: int = 2
+    n_parallel: int = 1 # 4 is better
+    algo: str = "sac"
+    n_epochs_per_eval: int = 100
+    n_epochs_per_save: int = 0
+    n_epochs_per_pt_save: int = 0
+    n_epochs_per_pkl_update: int = 0
+    n_epochs: int = 200001 # 16000 is better
+    eval_plot_axis: Optional[List[float]] = field(default_factory=lambda: [-50, 50, -50, 50])
+    trans_optimization_epochs: int = 50
+    te_trans_optimization_epochs: int = 50
+    sac_replay_buffer: int = 1
+    sac_max_buffer_size: int = 1000000
+    joint_train: int = 1
+    te_only_last_frame: int  = 0
+    goal_range: float = 7.5
+    alpha: float = 0.1
+    cp_multi_step: int = 25
+    downstream_reward_type: str = "esparse"
+    downstream_num_goal_steps: int = 50
+    cp_path: str = "exp/path/option_policy10000.pt"
+    cp_path_idx: int = 0
+    cp_unit_length: int = 1
 
-    sac_kwargs = dict(
-        learning_rate=1e-4,
-        buffer_size=int(1e5),
-        batch_size=256,
-        gamma=0.99,
-        tau=0.995,
-        ent_coef=0.01,
-        train_freq=1,
-        gradient_steps=100,
-        verbose=1,
-        seed=42,
-        device='auto',
+
+
+hl_kitchen_config = METRAHighLevelKitchenConfig()
+
+
+@wrap_experiment(log_dir=get_log_dir(hl_kitchen_config), name=get_exp_name(hl_kitchen_config)[0])
+def run(ctxt=None):
+    if hl_kitchen_config.n_thread is not None:
+        torch.set_num_threads(hl_kitchen_config.n_thread)
+
+    
+    def _finalize_lr(lr):
+        if lr is None:
+            lr = hl_kitchen_config.common_lr
+        else:
+            assert bool(lr), 'To specify a lr of 0, use a negative value'
+        if lr < 0.0:
+            dowel.logger.log(f'Setting lr to ZERO given {lr}')
+            lr = 0.0
+        return lr
+
+
+    set_seed(hl_kitchen_config.seed)
+
+    runner = OptionLocalRunner(ctxt)
+
+
+    all_tasks = ['bottom burner', 'top burner', 'light switch', 'slide cabinet', 'hinge cabinet', 'microwave', 'kettle']
+
+
+    env = KitchenEnv(
+        tasks_to_complete=all_tasks,
+        terminate_on_tasks_completed=True,
+        render_mode="rgb_array"
     )
 
-    model = SAC('MlpPolicy', hier_env, **sac_kwargs)
+    custom_order = [
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,     # Panda Arm and Gripper States
+                18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 40, 41, 42, 43, 44, 45, 46, 47, 48,  # Burners and Overhead Light
+                29, 30, 31, 49, 50, 51,                                           # Cabinets (Slide + Left + Right Hinge)
+                32, 52,                                                          # Microwave Door
+                33, 34, 35, 36, 37, 38, 39, 53, 54, 55, 56, 57, 58               # Kettle
+    ]
 
-    total_timesteps = 1_000_000
-    model.learn(total_timesteps=total_timesteps)
 
-    model.save("high_level_policy_sac")
 
-    obs = hier_env.reset()
-    done = False
+    max_path_length = hl_kitchen_config.max_path_length
+    if hl_kitchen_config.cp_path is not None:
+        max_path_length *= hl_kitchen_config.cp_multi_step
 
-    while not done:
-        skill, _ = model.predict(obs)
-        obs, reward, done, info = hier_env.step(skill)
-        # No need to call render, video will be saved automatically
 
-    print(f"Video saved to {video_dir}")
+    contextualized_make_env = functools.partial(lambda: env, args=hl_kitchen_config, max_path_length=max_path_length)
+    env = contextualized_make_env()
+
+    if hl_kitchen_config.cp_path is not None:
+        cp_path = hl_kitchen_config.cp_path
+        if not os.path.exists(cp_path):
+            import glob
+            cp_path = glob.glob(cp_path)[0]
+        cp_dict = torch.load(cp_path, map_location='cpu')
+
+        env = ChildPolicyEnv(
+            env,
+            cp_dict,
+            cp_action_range=1.5,
+            cp_unit_length=hl_kitchen_config.cp_unit_length,
+            cp_multi_step=hl_kitchen_config.cp_multi_step,
+            cp_num_truncate_obs=0,
+        )
+
+    obs_dim = env.spec.observation_space.flat_dim
+    action_dim = env.spec.action_space.flat_dim
+    module_obs_dim = obs_dim
+
+
+
+    if hl_kitchen_config.algo in ['sac', 'ppo']:
+        policy_q_input_dim = module_obs_dim # obs_dim (should be fixed)
+
+
+    if hl_kitchen_config.algo == "sac":
+        pass
+
+    device = torch.device('cuda' if hl_kitchen_config.use_gpu else 'cpu')
+    master_dims = [hl_kitchen_config.model_master_dim] * hl_kitchen_config.model_master_num_layers
+
+
+    if hl_kitchen_config.model_master_nonlinearity == 'relu':
+        nonlinearity = torch.relu
+    elif hl_kitchen_config.model_master_nonlinearity == 'tanh':
+        nonlinearity = torch.tanh
+    else:
+        nonlinearity = None
+
+
+
+    if hl_kitchen_config.algo == "sac":
+        qf1 = ContinuousMLPQFunctionEx(
+            obs_dim=policy_q_input_dim,
+            action_dim=action_dim,
+            hidden_sizes=master_dims,
+            hidden_nonlinearity=nonlinearity or torch.relu,
+        )
+        qf2 = ContinuousMLPQFunctionEx(
+            obs_dim=policy_q_input_dim,
+            action_dim=action_dim,
+            hidden_sizes=master_dims,
+            hidden_nonlinearity=nonlinearity or torch.relu,
+        )
+        log_alpha = ParameterModule(torch.Tensor([np.log(hl_kitchen_config.alpha)]))
+
+        vf = MLPModule(
+            input_dim=policy_q_input_dim,
+            output_dim=1,
+            hidden_sizes=master_dims,
+            hidden_nonlinearity=nonlinearity or torch.relu,
+            layer_normalization=hl_kitchen_config.critic_layer_norm,
+        )
+
+        optimizers.update({
+            'qf': torch.optim.Adam([
+                {'params': list(qf1.parameters()) + list(qf2.parameters()), 'lr': _finalize_lr(hl_kitchen_config.sac_lr_q)},
+            ]),
+            'log_alpha': torch.optim.Adam([
+                {'params': log_alpha.parameters(), 'lr': _finalize_lr(hl_kitchen_config.sac_lr_a)},
+            ]),
+            'vf': torch.optim.Adam([
+                {'params': vf.parameters(), 'lr': _finalize_lr(hl_kitchen_config.lr_op)},
+            ]),
+        })
+
+    elif hl_kitchen_config.algo == 'ppo':
+        vf = MLPModule(
+            input_dim=policy_q_input_dim,
+            output_dim=1,
+            hidden_sizes=master_dims,
+            hidden_nonlinearity=nonlinearity or torch.relu,
+            layer_normalization=hl_kitchen_config.critic_layer_norm,
+        )
+        optimizers = ({
+            'vf': torch.optim.Adam([
+                {'params': vf.parameters(), 'lr': _finalize_lr(hl_kitchen_config.lr_op)},
+            ]),
+        })
+
+
+    optimizer = OptimizerGroupWrapper(
+            optimizers=optimizers,
+            max_optimization_epochs=None,
+        )
+
+
+    algo_kwargs = dict(
+            env_name=hl_kitchen_config.env,
+            algo=hl_kitchen_config.algo,
+            env_spec=env.spec,
+            # option_policy=option_policy,
+            # traj_encoder=traj_encoder,
+            # skill_dynamics=skill_dynamics,
+            # dist_predictor=dist_predictor,
+            # dual_lam=dual_lam,
+            optimizer=optimizer,
+            alpha=hl_kitchen_config.alpha,
+            max_path_length=hl_kitchen_config.max_path_length,
+            n_epochs_per_eval=hl_kitchen_config.n_epochs_per_eval,
+            n_epochs_per_log=hl_kitchen_config.n_epochs_per_log, 
+            n_epochs_per_tb=hl_kitchen_config.n_epochs_per_log, 
+            n_epochs_per_save=hl_kitchen_config.n_epochs_per_save, 
+            n_epochs_per_pt_save=hl_kitchen_config.n_epochs_per_pt_save, 
+            n_epochs_per_pkl_update=hl_kitchen_config.n_epochs_per_eval if hl_kitchen_config.n_epochs_per_pkl_update is None else hl_kitchen_config.n_epochs_per_pkl_update,
+            dim_option=hl_kitchen_config.dim_option,
+            N = hl_kitchen_config.N,
+            num_random_trajectories=hl_kitchen_config.num_random_trajectories,
+            num_video_repeats=hl_kitchen_config.num_video_repeats,
+            eval_record_video=hl_kitchen_config.eval_record_video,
+            video_skip_frames=hl_kitchen_config.video_skip_frames,
+            eval_plot_axis=hl_kitchen_config.eval_plot_axis,
+            name='sac',
+            device=device,
+            sample_cpu=hl_kitchen_config.sample_cpu,
+            num_train_per_epoch=1,
+            sd_batch_norm=hl_kitchen_config.sd_batch_norm,
+            # skill_dynamics_obs_dim=skill_dynamics_obs_dim,
+            trans_minibatch_size=hl_kitchen_config.trans_minibatch_size,
+            trans_optimization_epochs=hl_kitchen_config.trans_optimization_epochs,
+            discount=hl_kitchen_config.sac_discount,
+            discrete=hl_kitchen_config.discrete,
+            unit_length=hl_kitchen_config.unit_length,
+        )
+    
+
+    replay_buffer = PathBufferEx(capacity_in_transitions=int(hl_kitchen_config.sac_max_buffer_size), pixel_shape=None)
+
+    
+    sac_args = dict(
+        qf1=qf1,
+        qf2=qf2,
+        log_alpha=log_alpha,
+        tau=hl_kitchen_config.sac_tau,
+        scale_reward=hl_kitchen_config.sac_scale_reward,
+        target_coef=hl_kitchen_config.sac_target_coef,
+
+        replay_buffer=replay_buffer,
+        min_buffer_size=hl_kitchen_config.sac_min_buffer_size,
+        inner=hl_kitchen_config.inner,
+
+        num_alt_samples=hl_kitchen_config.num_alt_samples,
+        split_group=hl_kitchen_config.split_group,
+
+        # dual_reg=hl_kitchen_config.dual_reg,
+        # dual_slack=hl_kitchen_config.dual_slack,
+        # dual_dist=hl_kitchen_config.dual_dist,
+
+        pixel_shape=None,
+        # partition_points=partition_points
+
+    )
+
+    if hl_kitchen_config.algo == "sac":
+        algo = SAC(**sac_args)
+        
+    elif hl_kitchen_config.algo == 'ppo':
+        algo = PPO(
+            **algo_kwargs,
+            vf=vf,
+            gae_lambda=0.95,
+            ppo_clip=0.2,
+        )
+    
+
+    if hl_kitchen_config.sample_cpu:
+        algo.option_policy.cpu()
+    else:
+        algo.option_policy.to(device)
+
+    runner.setup(
+            algo=algo,
+            env=env,
+            make_env=contextualized_make_env,
+            sampler_cls=OptionMultiprocessingSampler,
+            sampler_args=dict(n_thread=hl_kitchen_config.n_thread),
+            n_workers=hl_kitchen_config.n_parallel,
+        )
+
+    algo.option_policy.to(device)
+    runner.train(n_epochs=hl_kitchen_config.n_epochs, batch_size=hl_kitchen_config.traj_batch_size)
+
